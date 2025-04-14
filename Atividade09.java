099d73558f2c339bc43f3f71e8991f2a3172a6cf
rubenstvares <rubensgabrieltavaresferreira@gmail.com>
2025-04-14 20:24:21 -0300

    Add files via upload

---
+++ Atividade09.java
@@ -0,0 +1,9 @@
+public class Atividade09 {
+    public static void main(String[] args) {
+        // Laço for para imprimir os números entre 40 e 20 em ordem decrescente
+        for (int i = 39; i > 20; i--) {
+            System.out.println("Decrescente: " + i);  // Exibe o número atual
+        }
+    }
+}
+
