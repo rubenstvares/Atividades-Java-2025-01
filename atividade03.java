03114f715c5c8b883917f3f6ab699baf63939be4
rubenstvares <rubensgabrieltavaresferreira@gmail.com>
2025-04-14 20:05:04 -0300

    Add files via upload

---
+++ atividade03.java
@@ -0,0 +1,21 @@
+public class atividade03 {
+    public static void main(String[] args) {
+        // Declaração das variáveis
+        int a;           // Tipo inteiro
+        double b;        // Tipo real (números de ponto flutuante)
+        char c;          // Tipo caractere
+        boolean d;       // Tipo booleano
+
+        // Atribuindo valores às variáveis
+        a = 7;
+        b = 5.23;
+        c = 'B';         // Usando um caractere único
+        d = true;        // Pode ser verdadeiro (true) ou falso (false)
+
+        // Exibindo os valores das variáveis
+        System.out.println("Este é meu número Inteiro: " + a);
+        System.out.println("Este é meu número Real: " + b);
+        System.out.println("Este é meu caractere: " + c);
+        System.out.println("Este é meu valor booleano: " + d);
+    }
+}
