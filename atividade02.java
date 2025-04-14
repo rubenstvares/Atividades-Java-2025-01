9bb9faa9fae40b9c4f1985493161f96742ccdf3c
rubenstvares <rubensgabrieltavaresferreira@gmail.com>
2025-04-14 19:59:15 -0300

    Add files via upload

---
+++ atividade02.java
@@ -0,0 +1,18 @@
+public class atividade02 {
+    public static void main(String[] args) {
+        // Declaração das variáveis para armazenar os resultados das operações
+        double som, sub, div, mul;
+
+        // Realizando as operações
+        som = 12 + 3;
+        sub = 7 - 2;
+        div = 2 / 2.0; // Para garantir que a divisão seja feita com precisão decimal
+        mul = 2 * 3;
+
+        // Exibindo os resultados de cada operação
+        System.out.println("A soma de 12 + 3 é igual: " + som);
+        System.out.println("A subtração de 7 - 2 é: " + sub);
+        System.out.println("2 / 2 é: " + div);
+        System.out.println("2 * 3 é: " + mul);
+    }
+}
