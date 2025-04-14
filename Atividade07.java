b33c8026d61d845b2df633dadbe955905f8669e6
rubenstvares <rubensgabrieltavaresferreira@gmail.com>
2025-04-14 20:21:14 -0300

    Add files via upload

---
+++ Atividade07.java
@@ -0,0 +1,12 @@
+public class Atividade07 {
+    public static void main(String[] args) {
+        // Declaração da variável contador
+        int cont = 101;  // Começamos do número 101, pois 100 não será impresso
+
+        // Laço while para imprimir os números entre 100 e 200
+        while (cont < 200) {
+            System.out.println("Contagem: " + cont);
+            cont++;  // Incrementa 1 ao contador
+        }
+    }
+}
